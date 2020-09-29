8ab016005bda96ab78c8ec35ace1a0fbc9351e5f
Marcos <marsanben92@gmail.com>
2020-09-30 00:42:40 +0200

    polish: server bind params

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.14.6-alpine3.12 AS builder
+FROM golang:1.15.2-alpine3.12 AS builder
 
 RUN apk update && \
     apk add --no-cache --update make bash ca-certificates && \
@@ -10,10 +10,14 @@ COPY . .
 RUN go mod download
 RUN make build-docker
 
-FROM scratch
+FROM alpine:3.12
 
 COPY --from=builder /app/build/*/* /usr/local/bin/
 COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
 
 EXPOSE 7373
-ENTRYPOINT [ "/usr/local/bin/server" ]
+
+ENV VISIGOTH_TCP_BIND_TO=:7374
+ENV VISIGOTH_HTTP_BIND_TO=7373
+
+CMD /usr/local/bin/server -tcp $VISIGOTH_TCP_BIND_TO -http $VISIGOTH_HTTP_BIND_TO
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"fmt"
 	"github.com/sonirico/visigoth/pkg/entities"
 	"log"
 	"net/http"
@@ -23,21 +22,17 @@ type healthIndex struct {
 }
 
 func init() {
-	var tcpPort, httpPort string
-	flag.StringVar(&httpPort, "http", "7374", "HTTP port to bind to")
-	flag.StringVar(&tcpPort, "tcp", "7373", "TCP port to bind to")
+	flag.StringVar(&bindToHttp, "http", "localhost:7374", "HTTP port to bind to")
+	flag.StringVar(&bindToTcp, "tcp", "localhost:7373", "TCP port to bind to")
 	flag.Parse()
 
-	if strings.Compare(strings.TrimSpace(httpPort), "") == 0 {
+	if strings.Compare(strings.TrimSpace(bindToHttp), "") == 0 {
 		log.Fatal("-http parameter is required")
 	}
 
-	if strings.Compare(strings.TrimSpace(tcpPort), "") == 0 {
+	if strings.Compare(strings.TrimSpace(bindToTcp), "") == 0 {
 		log.Fatal("-tcp parameter is required")
 	}
-
-	bindToTcp = fmt.Sprintf("localhost:%s", tcpPort)
-	bindToHttp = fmt.Sprintf(":%s", httpPort)
 }
 
 func main() {
